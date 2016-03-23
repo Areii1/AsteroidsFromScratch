e4cba8cbce4d7a2dfd7c9182b09fceceab126ae1
Areii1 <ari.jaaskelainen1@gmail.com>
2016-03-23 06:17:34 +0200

    Created basic game running functionality, Outputs a black screen.

---
+++ src/game/Window.java
@@ -0,0 +1,28 @@
+package game;
+
+import java.awt.Dimension;
+import javax.swing.JFrame;
+
+public class Window extends javax.swing.JFrame {
+
+	private static final long serialVersionUID = -4369730830015653927L;
+	
+	public Window(int width, int height, String title, Game game) {
+		JFrame frame = new JFrame();
+		
+		frame.setTitle(title);
+		Dimension size = new Dimension(width, height);
+		
+		frame.setMinimumSize(size);
+		frame.setPreferredSize(size);
+		frame.setMaximumSize(size);
+		
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		
+		frame.add(game);
+		frame.setVisible(true);
+		game.start();
+	}
+}
